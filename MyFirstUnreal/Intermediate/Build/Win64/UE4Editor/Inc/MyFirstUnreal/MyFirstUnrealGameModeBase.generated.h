d3ca69bf52080c18f8393777258e837bc763923d
areong9292 <areong9292@gmail.com>
2020-06-09 23:15:24 +0900

    fixed untracked files

---
+++ MyFirstUnreal/Intermediate/Build/Win64/UE4Editor/Inc/MyFirstUnreal/MyFirstUnrealGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MYFIRSTUNREAL_MyFirstUnrealGameModeBase_generated_h
+#error "MyFirstUnrealGameModeBase.generated.h already included, missing '#pragma once' in MyFirstUnrealGameModeBase.h"
+#endif
+#define MYFIRSTUNREAL_MyFirstUnrealGameModeBase_generated_h
+
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_RPC_WRAPPERS
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyFirstUnrealGameModeBase(); \
+	friend MYFIRSTUNREAL_API class UClass* Z_Construct_UClass_AMyFirstUnrealGameModeBase(); \
+public: \
+	DECLARE_CLASS(AMyFirstUnrealGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/MyFirstUnreal"), NO_API) \
+	DECLARE_SERIALIZER(AMyFirstUnrealGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyFirstUnrealGameModeBase(); \
+	friend MYFIRSTUNREAL_API class UClass* Z_Construct_UClass_AMyFirstUnrealGameModeBase(); \
+public: \
+	DECLARE_CLASS(AMyFirstUnrealGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/MyFirstUnreal"), NO_API) \
+	DECLARE_SERIALIZER(AMyFirstUnrealGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyFirstUnrealGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyFirstUnrealGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyFirstUnrealGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyFirstUnrealGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyFirstUnrealGameModeBase(AMyFirstUnrealGameModeBase&&); \
+	NO_API AMyFirstUnrealGameModeBase(const AMyFirstUnrealGameModeBase&); \
+public:
+
+
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyFirstUnrealGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyFirstUnrealGameModeBase(AMyFirstUnrealGameModeBase&&); \
+	NO_API AMyFirstUnrealGameModeBase(const AMyFirstUnrealGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyFirstUnrealGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyFirstUnrealGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyFirstUnrealGameModeBase)
+
+
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_12_PROLOG
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_RPC_WRAPPERS \
+	MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_INCLASS \
+	MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MyFirstUnreal_Source_MyFirstUnreal_MyFirstUnrealGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ MyFirstUnreal/Source/MyFirstUnreal/MyFirstUnrealGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyFirstUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYFIRSTUNREAL_API AMyFirstUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
