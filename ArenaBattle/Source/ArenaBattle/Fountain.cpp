d6f8c4b09a89fd9193cb76b7b23d07b7e6eac204
areong9292 <areong9292@gmail.com>
2020-06-16 20:22:26 +0900

    Chpater 02.

--- ArenaBattle/Source/ArenaBattle/Fountain.cpp
+++ ArenaBattle/Source/ArenaBattle/Fountain.cpp
@@ -9,6 +9,56 @@ AFountain::AFountain()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Component 생성
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
+	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
+
+	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
+	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+
+	// 루트 컴포넌트로 Body 지정, Body밑에 Water를 자식으로 붙힘
+	RootComponent = Body;
+	Water->SetupAttachment(Body);
+	Light->SetupAttachment(Body);
+	Splash->SetupAttachment(Body);
+
+	// F 접두사 - 언리얼 오브젝트와 관련 없는 일반 c++ 클래스, 구조체
+	Water->SetRelativeLocation(FVector(0.0f, 0.0f, 135.0f));
+	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+
+	// 에셋 경로로 에셋 부르기
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01"));
+
+	// 부르기 성공했으면
+	if (SM_BODY.Succeeded())
+	{
+		// SetStaticMesh에 에셋의 포인터(SM_BODY.Object)를 전달한다
+		Body->SetStaticMesh(SM_BODY.Object);
+	}
+
+	// 에셋 경로로 에셋 부르기
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02"));
+
+	// 부르기 성공했으면
+	if (SM_WATER.Succeeded())
+	{
+		// SetStaticMesh에 에셋의 포인터(SM_BODY.Object)를 전달한다
+		Water->SetStaticMesh(SM_WATER.Object);
+	}
+
+	// 에셋 경로로 에셋 부르기
+	static ConstructorHelpers::FObjectFinder<UParticleSystem>
+		PS_SLASH(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01"));
+
+	// 부르기 성공했으면
+	if (PS_SLASH.Succeeded())
+	{
+		// SetStaticMesh에 에셋의 포인터(SM_BODY.Object)를 전달한다
+		Splash->SetTemplate(PS_SLASH.Object);
+	}
 }
 
 // Called when the game starts or when spawned
--- ArenaBattle/Source/ArenaBattle/Fountain.h
+++ ArenaBattle/Source/ArenaBattle/Fountain.h
@@ -2,10 +2,15 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h"
 #include "GameFramework/Actor.h"
+
+// ==자동화 매크로== 클래스 분석 시 필요 정보 헤더
 #include "Fountain.generated.h"
 
+// ==클래스 선언 매크로== UCLASS(), GENERATED_BODY()
+// ==클래스 선언 접두사== AFountain, Actor면 A, 아니면 U
+// ==외부 모듈에의 공개여부== ARENABATTLE_API, 있으면 접근가능
 UCLASS()
 class ARENABATTLE_API AFountain : public AActor
 {
@@ -22,7 +27,22 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
+	// UPROPERTY() - 언리얼 엔진에서 자동으로 메모리 관리해준다
+	// 단, 언리얼 객체만 가능하고 새로만든 일반 객체도 자동화해주려면
+	// 특정 매크로를 선언해주어야한다 - ==자동화 매크로==
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent *Body;
 
-	
-	
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent *Water;
+
+	UPROPERTY(VisibleAnywhere)
+	UPointLightComponent *Light;
+
+	UPROPERTY(VisibleAnywhere)
+	UParticleSystemComponent *Splash;
+
+	UPROPERTY(EditAnywhere, Category=ID)
+	int32 ID;
 };
